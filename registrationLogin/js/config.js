76a8a14f9db7e49d386d035f6bb14c963c782961
PrinceKrampah <jsksprince@gmail.com>
2022-02-14 12:42:18 +0300

    Registration and login system

---
+++ registrationLogin/js/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCAxRXQuSElY4bYDAWwUeCLmuDrslYkpXI",
+    authDomain: "fir-basics-ee5d6.firebaseapp.com",
+    projectId: "fir-basics-ee5d6",
+    storageBucket: "fir-basics-ee5d6.appspot.com",
+    messagingSenderId: "974342515441",
+    appId: "1:974342515441:web:10ee926475e987fbad5750",
+    measurementId: "G-VPWBW26DF4"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
